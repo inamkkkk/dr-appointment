1c2124e495175f757338319249916c948e9e7cbf
inamkkkk <inamk9025@gmail.com>
2025-10-05 04:45:48 +0500

    fix: refine src/routes/index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -14,8 +14,9 @@ const defaultRoutes = [
   { path: '/patients', route: patientRoutes },
   { path: '/appointments', route: appointmentRoutes },
   { path: '/webhooks', route: webhookRoutes },
-  { path: '/context', route: contextRoutes },
-];
+  { path: '/context', route: contextRoutes }];
+
+// TODO: Implement route registration logic if needed, otherwise this loop is sufficient.
 
 defaultRoutes.forEach((route) => {
   router.use(route.path, route.route);
