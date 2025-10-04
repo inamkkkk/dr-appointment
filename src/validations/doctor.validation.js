2bb7668103d10622c52fe40d29a3f9edafa4f2a6
inamkkkk <inamk9025@gmail.com>
2025-10-05 04:51:36 +0500

    fix: refine src/validations/doctor.validation.js

--- src/validations/doctor.validation.js
+++ src/validations/doctor.validation.js
@@ -3,14 +3,11 @@ const { objectId } = require('./custom.validation');
 
 const getDoctor = {
   params: Joi.object().keys({
-    id: Joi.string().custom(objectId),
-  }),
-};
+    id: Joi.string().custom(objectId)})};
 
 const updateDoctor = {
   params: Joi.object().keys({
-    id: Joi.string().custom(objectId),
-  }),
+    id: Joi.string().custom(objectId)}),
   body: Joi.object()
     .keys({
       name: Joi.string(),
@@ -30,14 +27,25 @@ const updateDoctor = {
         Joi.object({
           name: Joi.string().required(),
           content: Joi.string().required(),
-          language: Joi.string(),
-        })
-      ),
-    })
-    .min(1),
-};
+          language: Joi.string()})
+      )})
+    .min(1)};
+
+// TODO: Add validation for creating a doctor
+const createDoctor = {
+  body: Joi.object().keys({
+    name: Joi.string().required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().required(), // Assuming password is required for creation
+    specialization: Joi.string().required(),
+    phone: Joi.string(),
+    whatsapp_number: Joi.string(),
+    // Add other fields if necessary for doctor creation
+  })};
+
 
 module.exports = {
   getDoctor,
   updateDoctor,
+  createDoctor, // Export the new validation
 };
