d526c04c84a3f677a3ffa84e398d73d3d84ab5e7
inamkkkk <inamk9025@gmail.com>
2025-10-05 04:48:58 +0500

    fix: refine src/controllers/appointment.controller.js

--- src/controllers/appointment.controller.js
+++ src/controllers/appointment.controller.js
@@ -16,18 +16,21 @@ const logger = pino({
 
 const createAppointment = catchAsync(async (req, res) => {
   logger.info('Creating new appointment.');
+  // TODO: Add input validation for the create appointment request body.
   const appointment = await queryOrchestrator.createAppointment(req.body);
   res.status(httpStatus.CREATED).send(appointment);
 });
 
 const getAppointments = catchAsync(async (req, res) => {
   logger.info('Fetching appointments.');
+  // TODO: Implement filtering and pagination for the get appointments request.
   const appointments = await appointmentService.queryAppointments();
   res.send(appointments);
 });
 
 const getAppointment = catchAsync(async (req, res) => {
   logger.info('Fetching appointment by ID:', req.params.id);
+  // TODO: Add validation for the appointment ID parameter.
   const appointment = await appointmentService.getAppointmentById(req.params.id);
   if (!appointment) {
     throw new ApiError(httpStatus.NOT_FOUND, 'Appointment not found');
@@ -37,6 +40,8 @@ const getAppointment = catchAsync(async (req, res) => {
 
 const updateAppointment = catchAsync(async (req, res) => {
   logger.info('Updating appointment by ID:', req.params.id);
+  // TODO: Add input validation for the update appointment request body.
+  // TODO: Add validation for the appointment ID parameter.
   const appointment = await appointmentService.updateAppointmentById(req.params.id, req.body);
   res.send(appointment);
 });
@@ -45,5 +50,4 @@ module.exports = {
   createAppointment,
   getAppointments,
   getAppointment,
-  updateAppointment,
-};
+  updateAppointment};
