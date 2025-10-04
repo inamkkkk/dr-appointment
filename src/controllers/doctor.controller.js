c0eb9296a8a0bfba97d04b78a4176cdf4539989e
inamkkkk <inamk9025@gmail.com>
2025-10-05 04:48:19 +0500

    fix: refine src/controllers/doctor.controller.js

--- src/controllers/doctor.controller.js
+++ src/controllers/doctor.controller.js
@@ -16,18 +16,21 @@ const logger = pino({
 
 const createDoctor = catchAsync(async (req, res) => {
   logger.info('Creating new doctor:', req.body.email);
+  // TODO: Implement input validation for doctor creation
   const doctor = await doctorService.createDoctor(req.body);
   res.status(httpStatus.CREATED).send(doctor);
 });
 
 const getDoctors = catchAsync(async (req, res) => {
   logger.info('Fetching all doctors.');
+  // TODO: Implement pagination and filtering for doctor list
   const doctors = await doctorService.queryDoctors();
   res.send(doctors);
 });
 
 const getDoctor = catchAsync(async (req, res) => {
   logger.info('Fetching doctor by ID:', req.params.id);
+  // TODO: Validate req.params.id format
   const doctor = await doctorService.getDoctorById(req.params.id);
   if (!doctor) {
     throw new ApiError(httpStatus.NOT_FOUND, 'Doctor not found');
@@ -37,12 +40,15 @@ const getDoctor = catchAsync(async (req, res) => {
 
 const updateDoctor = catchAsync(async (req, res) => {
   logger.info('Updating doctor by ID:', req.params.id);
+  // TODO: Implement input validation for doctor update
+  // TODO: Prevent updating fields like 'password' or 'role' directly via this endpoint
   const doctor = await doctorService.updateDoctorById(req.params.id, req.body);
   res.send(doctor);
 });
 
 const deleteDoctor = catchAsync(async (req, res) => {
   logger.info('Deleting doctor by ID:', req.params.id);
+  // TODO: Validate req.params.id format
   await doctorService.deleteDoctorById(req.params.id);
   res.status(httpStatus.NO_CONTENT).send();
 });
@@ -52,5 +58,4 @@ module.exports = {
   getDoctors,
   getDoctor,
   updateDoctor,
-  deleteDoctor,
-};
+  deleteDoctor};
